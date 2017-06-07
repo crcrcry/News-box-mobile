20ae001eb07d5d3cef1f1a222ac8e0b3d2439881
crcrcry <870247639@qq.com>
2017-06-07 19:11:15 +0800

    fix bugs

--- index.ios.js
+++ index.ios.js
@@ -11,30 +11,11 @@ import Nav from './src/components/Nav/';
 export default class crcrcryNews extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'column'}}>
-        <View style={styles.title}>
-          <Text style={styles.titleText}>Crcrcry-News</Text>
-        </View>
-        <View style={{flex: 1}}>
-          <Nav />
-        </View>
+      <View style={{flex: 1}}>
+        <Nav />
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  title: {
-    width: '100%',
-    height: 30,
-    marginTop: 24,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  titleText: {
-    fontSize: 17,
-  }
-})
 
 AppRegistry.registerComponent('crcrcryNews', () => crcrcryNews);
